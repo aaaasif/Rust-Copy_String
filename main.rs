b9531abe603971adcd56fde05beece6face2f62f
Abdullah Al Asif <alexasif4@gmail.com>
2023-12-30 18:36:22 +0800

    uses of Clone

--- main.rs
+++ main.rs
@@ -14,7 +14,13 @@ fn main() {
     println ("{}", y)
 }
 
-
+fn main() {
+    let x= string::from("EzSnippet"); 
+    let y = x.clone();
+    // to copy string value we need to use clone()
+    println ("{}", x)
+    println ("{}", y)
+}
 
  
 
