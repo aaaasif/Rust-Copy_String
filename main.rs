e28c82da0ae1bdac1add99cd0e19b1f01e59cd26
Abdullah Al Asif <alexasif4@gmail.com>
2024-01-01 18:36:14 +0800

    write a small application with rust

--- main.rs
+++ main.rs
@@ -22,5 +22,14 @@ fn main() {
     println ("{}", y)
 }
 
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
+}
+
  
 
